b0f94b7eb770f51c6fceb3d7498072cf9b7d22df
jeewoon.han <jeewoon.han@navercorp.com>
2022-01-26 14:51:09 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMMTester"
+include(":androidApp")
+include(":shared")
